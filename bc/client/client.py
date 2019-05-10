d105477507cc876e4026c4a41e273ce17006538c
bmark <bense.mark@gmail.com>
2019-05-10 18:45:34 +0200

    Continued implementing the client.

--- bc/client/client.py
+++ bc/client/client.py
@@ -13,49 +13,60 @@ def generateMsg(req_gen: RequestGenerator, task) -> bytes:
     return req.to_bytes()
 
 if __name__ == '__main__':
-    prompt = '''Please choose an option:
-        1 - list my appointments
-        2 - list available appointments
-        3 - list basket
-        4 - add appointment to basket
-        5 - remove appointment form basket
-        6 - confirm booking
-        7 - cancel appointment
-        q - quit
-    '''
-    req_gen = RequestGenerator(1) #TODO
-    optn = {'1': req_gen.msg_list_booked_appointments,
-            '2': req_gen.msg_list_available_appointments,
-            '3': req_gen.msg_list_basket,
-            '4': req_gen.msg_add_appointment_to_basket,
-            '5': req_gen.msg_remove_appointment_from_basket,
-            '6': req_gen.msg_confirm_booking,
-            '7': req_gen.msg_cancel_appointment}
+
+    providers = ['fodrasz', 'kozmetikus', 'manikur']
     while True:
-        task = input(prompt)
-        if task not in optn:
-            print('{} is not an available option.'.format(task))
-        elif task == 'q':
-            break
+        provider = input('Please enter a providers name:')
+        if provider not in providers:
+            print('{} is not an available option.')
         else:
-            host, port = '', 1 #TODO
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                sock.connect((host, port))
-                trans = Transceiver(sock)
-                msg = generateMsg(req_gen, task)
-                trans.send(msg)
-                ans = trans.receive()
+            print('{} selected')
 
-                ans_data = Request(ans).data()
 
-                ok = ans_data["OK"]
-                text = ans_data["text"]
+        prompt = '''Please choose an option:
+            1 - list my appointments
+            2 - list available appointments
+            3 - list basket
+            4 - add appointment to basket
+            5 - remove appointment form basket
+            6 - confirm booking
+            7 - cancel appointment
+            q - quit
+        '''
 
-                if ok:
-                    print('Your request had been confirmed.')
-                else:
-                    print('Your request could not be confirmed due to the following error: ' +
-                    text)
+        req_gen = RequestGenerator(1) #TODO
+        optn = {'1': req_gen.msg_list_booked_appointments,
+                '2': req_gen.msg_list_available_appointments(provider),
+                '3': req_gen.msg_list_basket,
+                '4': req_gen.msg_add_appointment_to_basket,
+                '5': req_gen.msg_remove_appointment_from_basket,
+                '6': req_gen.msg_confirm_booking,
+                '7': req_gen.msg_cancel_appointment}
+        while True:
+            task = input(prompt)
+            if task not in optn:
+                print('{} is not an available option.'.format(task))
+            elif task == 'q':
+                break
+            else:
+                host, port = '', 1 #TODO
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                    sock.connect((host, port))
+                    trans = Transceiver(sock)
+                    msg = generateMsg(req_gen, task)
+                    trans.send(msg)
+                    ans = trans.receive()
+
+                    ans_data = Request(ans).data()
+
+                    ok = ans_data["OK"]
+                    text = ans_data["text"]
+
+                    if ok:
+                        print('Your request had been confirmed.')
+                    else:
+                        print('Your request could not be confirmed due to the following error: ' +
+                        text)
 
 
 
