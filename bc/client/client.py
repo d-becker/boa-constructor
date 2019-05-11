d82081d521c88f039758757385efed4832697223
bmark <bense.mark@gmail.com>
2019-05-11 17:21:10 +0200

    Finished refactoring the client.

--- bc/client/client.py
+++ bc/client/client.py
@@ -8,17 +8,8 @@ from bc.common.entities import Appointment, TimeSlot, ServiceProvider
 
 
 def client_main():
-    while True:
-        task = main_prompt()
-
-        # optn = {'1': req_gen.msg_list_booked_appointments,
-        #         '2': req_gen.msg_list_available_appointments(provider),
-        #         '3': req_gen.msg_list_basket,
-        #         '4': req_gen.msg_add_appointment_to_basket,
-        #         '5': req_gen.msg_remove_appointment_from_basket,
-        #         '6': req_gen.msg_confirm_booking,
-        #         '7': req_gen.msg_cancel_appointment}
-
+    client = Client()
+    client.start()
 
 class Client:
     def __init__(self):
@@ -34,6 +25,19 @@ class Client:
             '6': self.handle_confirm_booking,
             '7': self.handle_cancel_appointment
             }
+    def start(self) -> None:
+        while True:
+            task = self.main_prompt()
+
+            if task == 'q':
+                print("Exiting.")
+                return
+
+            if task not in self.task_handlers:
+                print("Invalid task.")
+                continue
+
+            self.task_handlers[task]()
 
     def handle_list_booked_appointments(self) -> None:
         request = self.req_gen.msg_list_booked_appointments()
@@ -123,8 +127,7 @@ class Client:
             5 - remove appointment form basket
             6 - confirm booking
             7 - cancel appointment
-            q - quit\n
-        '''
+            q - quit\n'''
 
         task = input(prompt)
         return task
@@ -164,77 +167,6 @@ class Client:
         ts = TimeSlot(time_slot_ints[0], time_slot_ints[1], time_slot_ints[2], time_slot_ints[3])
         appointment = Appointment(ServiceProvider(provider_name), ts)
         return appointment
-
-def generateMsg(req_gen: RequestGenerator, task) -> bytes:
-    if task == '2':
-        prompt = '''Please type in the name of a service provider.
-            If you want to list all the available timeslots at every provider, please press ENTER.'''
-        prov = input(prompt)
-        if prov != '':
-            req = optn[task](prov)
-            return req.to_bytes()
-    req = optn[task]()
-    return req.to_bytes()
-
-def main_old():
-    providers = ['fodrasz', 'kozmetikus', 'manikur']
-    while True:
-        provider = input('Please enter a providers name:')
-        if provider not in providers:
-            print('{} is not an available option.')
-        else:
-            print('{} selected')
-
-
-        prompt = '''Please choose an option:
-            1 - list my appointments
-            2 - list available appointments
-            3 - list basket
-            4 - add appointment to basket
-            5 - remove appointment form basket
-            6 - confirm booking
-            7 - cancel appointment
-            q - quit
-        '''
-
-        req_gen = RequestGenerator(1) #TODO
-        optn = {'1': req_gen.msg_list_booked_appointments,
-                '2': req_gen.msg_list_available_appointments(provider),
-                '3': req_gen.msg_list_basket,
-                '4': req_gen.msg_add_appointment_to_basket,
-                '5': req_gen.msg_remove_appointment_from_basket,
-                '6': req_gen.msg_confirm_booking,
-                '7': req_gen.msg_cancel_appointment}
-        while True:
-            task = input(prompt)
-            if task not in optn:
-                print('{} is not an available option.'.format(task))
-            elif task == 'q':
-                break
-            else:
-                host, port = '', 1 #TODO
-                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-                    sock.connect((host, port))
-                    trans = Transceiver(sock)
-                    msg = generateMsg(req_gen, task)
-                    trans.send(msg)
-                    ans = trans.receive()
-
-                    ans_data = Message(ans).data()
-
-                    ok = ans_data["OK"]
-                    text = ans_data["text"]
-
-                    if ok:
-                        print('Your request had been confirmed.')
-                    else:
-                        print('Your request could not be confirmed due to the following error: ' +
-                        text)
-
-
-if __name__ == '__main__':
-    main()
-
 
 
 
